05e7674052a0bcc1f48ebe906aef68b23263504a
RandomizedRaptor <112107879+RandomizedRaptor@users.noreply.github.com>
2023-09-28 20:58:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+let chapters = {
+    debut: {
+        titre : `Batman: le retour du Sphinx`,
+        description: `Le Sphinx tient un immeuble en otage avec une bombe. Le symbol de Batman apparait dans le ciel.`,
+        image: './assets/images/logo.png',
+        boutons: [
+            {titre: `Repondre a l'appel`, destination: `enigme`}
+        ]
+    },
+    enigme: {
+        titre : `L'enigme`,
+        description: 'Le Sphinx vous donne une enigme a resoudre.',
+        image: './assets/images/enigme.webp',
+        boutons: [
+            {titre: `Abandonner`, destination: `reputation`},
+            {titre: `Resoudre l'enigme`, destination: `compte`},
+            {titre: `Analyser l'immeuble`, destination: `secret`}
+        ]
+    },
+    reputation: {
+        titre : `Votre reputation est ruinee`,
+        description: ' ',
+        image: './assets/images/reputation.webp',
+        boutons: [
+            {titre: `Recommencer`, destination: `debut`}
+        ]
+    },
+    compte: {
+        titre : `Compte a rebours`,
+        description: `Plus loin dans l'immeuble, un ecran afficheun compte a rebours.`,
+        image: './assets/images/compte.jpg',
+        boutons: [
+            {titre: `Continuer`, destination: `bombe`}
+        ]
+    },
+    secret: {
+        titre : `La carte`,
+        description: `Vous trouvez la carte de l'immeuble!`,
+        image: './assets/images/carte.jpg',
+        boutons: [
+            {titre: `Retourner`, destination: `debut`}
+        ]
+    },
+    bombe: {
+        titre : `Il y a une bombe`,
+        description: `La bombe est sur le point d'exploser!`,
+        image: './assets/images/bombe.webp',
+        boutons: [
+            {titre: `Fuir`, destination: `reputation`},
+            {titre: `Desamorcer la bombe`, destination: `sphinx`},
+            {titre: `Appeler a l'aide`, destination: `mort`}
+        ]
+    },
+    sphinx: {
+        titre : `Le Sphinx`,
+        description: 'Il se cache derriere une barriere, il y a une sortie en arriere de lui.',
+        image: './assets/images/',
+        boutons: [
+            {titre: `Continuer`, destination: `enigmeFinal`}
+        ]
+    },
+    mort: {
+        titre : `Ouch!`,
+        description: `Il n'y a pas assez de temps pour recevoir de l'aide. Vous mourrez...`,
+        image: './assets/images/',
+        boutons: [
+            {titre: `Recommencer`, destination: `debut`}
+        ]
+    },
+    enigmeFinal: {
+        titre : `L'enigme final`,
+        description: 'Le Sphinx vous donne une enigme final a resoudre.',
+        image: './assets/images/enigmeFinal',
+        boutons: [
+            {titre: `Resoudre l'enigme`, destination: `sphinxConfusion`},
+            {titre: `Affronter le Sphinx (grace a la carte)`, destination: `victoire`},
+            {titre: `Le convaincre de se rendre`, destination: `sphinxRefuse`}
+        ]
+    },
+    sphinxConfusion: {
+        titre : `Confusion`,
+        description: `Le Sphinx s'enfuit durant la confusion.`,
+        image: './assets/images/confusion',
+        boutons: [
+            {titre: `Recommencer`, destination: `debut`}
+        ]
+    },
+    victoire: {
+        titre : `Victoire!`,
+        description: 'Vous trouver, battez et capturer le Sphinx!',
+        image: './assets/images/batman',
+        boutons: [
+            {titre: `Recommencer`, destination: `debut`}
+        ]
+    },
+    sphinxRefuse: {
+        titre : `Il... refuse?`,
+        description: `Le Sphinx refuse et s'enfuit`,
+        image: './assets/images/confusion',
+        boutons: [
+            {titre: `Recommencer`, destination: `debut`}
+        ]
+    },
+};
+ 
+function goToChapter(chapter) {
+    if (typeof chapter === 'object') {
+        console.log(chapter.titre);
+        console.log(chapter.description);
+        console.log(chapter.boutons);
+    } else {
+        console.log('Erreur 🤓');
+}};
+
+goToChapter(chapters.debut);
